9eae9910c1349fc5bb1690c9f89e6bf3b5389580
kunzjar <kunzjar3@isu.edu>
2015-11-19 18:56:09 -0700

    First Edition

---
+++ MatchConsts.py
@@ -0,0 +1,12 @@
+URL = {
+    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
+    'match_by_Id': 'v{version}/match/{matchId}'
+}
+
+API_VERSIONS = {
+    'match': '2.2'
+}
+
+REGIONS = {
+    'north_america': 'na'
+}
